021bdd8bc688b4798d7c60b90606cbf436311423
manotim <manotimike@gmail.com>
2023-12-05 07:21:45 +0300

    run pip freeze to include dj_database_url

--- quizz_project/settings.py
+++ quizz_project/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kd==1%!_j@f8be6pkmklts_ddc5sn+%4b4@w%j8x&&961oc20y'
-
+# SECRET_KEY = 'django-insecure-kd==1%!_j@f8be6pkmklts_ddc5sn+%4b4@w%j8x&&961oc20y'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-kd==1%!_j@f8be6pkmklts_ddc5sn+%4b4@w%j8x&&961oc20y')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True')=='True'
 
